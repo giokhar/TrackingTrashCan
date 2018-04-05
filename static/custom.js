dd21db6685af3d81e8ad128ffcd19082d04a572b
Giorgi <giorgi.kharshila@gmail.com>
2018-04-05 01:37:27 -0400

    getting chart data in more accurate format

--- static/custom.js
+++ static/custom.js
@@ -8,8 +8,26 @@ function updateData(){
     
     if (data.length != parseInt($("#data").text())) {
     	$("#data").text(data.length);
+
+    	for(var i=0; i<data.length; i++){
+    		// Depth in CM
+    		var depth = data[i][0];
+    		console.log("Depth in cm = " + depth);
+
+    		// Date
+    		var date = new Date(data[i][1].$date);
+    		console.log("Date : " + date);
+    	}
     }
 
 	});
 	setTimeout(function(){updateData()}, 5000);
 }
+
+
+$.getJSON('http://127.0.0.1:5000/current', function(data) {
+
+	var date = new Date(data[1].$date);
+
+	console.log(date);
+});
