c26434b3fd07a1fc89e8ca7806f343f15857251d
Giorgi <giorgi.kharshila@gmail.com>
2018-04-09 22:52:42 -0400

    both charts working

--- static/custom.js
+++ static/custom.js
@@ -1,4 +1,85 @@
 
+Chart.pluginService.register({
+        beforeDraw: function (chart) {
+            if (chart.config.options.elements.center) {
+        //Get ctx from string
+        var ctx = chart.chart.ctx;
+        
+                //Get options from the center object in options
+        var centerConfig = chart.config.options.elements.center;
+        var fontStyle = centerConfig.fontStyle || 'Arial';
+                var txt = centerConfig.text;
+        var color = centerConfig.color || '#000';
+        var sidePadding = centerConfig.sidePadding || 20;
+        var sidePaddingCalculated = (sidePadding/100) * (chart.innerRadius * 2)
+        //Start with a base font of 30px
+        ctx.font = "30px " + fontStyle;
+        
+                //Get the width of the string and also the width of the element minus 10 to give it 5px side padding
+        var stringWidth = ctx.measureText(txt).width;
+        var elementWidth = (chart.innerRadius * 2) - sidePaddingCalculated;
+
+        // Find out how much the font can grow in width.
+        var widthRatio = elementWidth / stringWidth;
+        var newFontSize = Math.floor(30 * widthRatio);
+        var elementHeight = (chart.innerRadius * 2);
+
+        // Pick a new font size so it will not be larger than the height of label.
+        var fontSizeToUse = Math.min(newFontSize, elementHeight);
+
+                //Set font settings to draw it correctly.
+        ctx.textAlign = 'center';
+        ctx.textBaseline = 'middle';
+        var centerX = ((chart.chartArea.left + chart.chartArea.right) / 2);
+        var centerY = ((chart.chartArea.top + chart.chartArea.bottom) / 2);
+        ctx.font = fontSizeToUse+"px " + fontStyle;
+        ctx.fillStyle = color;
+        
+        //Draw text in center
+        ctx.fillText(txt, centerX, centerY);
+            }
+        }
+    });
+
+
+        var config = {
+            type: 'doughnut',
+            data: {
+                labels: [
+                  "Red",
+                  "Green",
+                  "Yellow"
+                ],
+                datasets: [{
+                    data: [300, 50, 100],
+                    backgroundColor: [
+                      "#FF6384",
+                      "#36A2EB",
+                      "#FFCE56"
+                    ],
+                    hoverBackgroundColor: [
+                      "#FF6384",
+                      "#36A2EB",
+                      "#FFCE56"
+                    ]
+                }]
+            },
+        options: {
+            elements: {
+                center: {
+                    text: '90%',
+          color: '#FF6384', // Default is #000000
+          fontStyle: 'Arial', // Default is Arial
+          sidePadding: 20 // Defualt is 20 (as a percentage)
+                }
+            }
+        }
+    };
+
+
+var ctx_two = document.getElementById("Donut").getContext("2d");
+var Donut = new Chart(ctx_two, config);
+
 var depths = [];
 var dates  = [];
 var link = window.location.protocol+"//"+window.location.hostname;
@@ -7,11 +88,10 @@ if (window.location.port) {
     link += ":"+window.location.port;
 }
 
-
 function main(){
 
-    var ctx = document.getElementById("myChart").getContext('2d');
-    var myChart = new Chart(ctx, {
+    var ctx_one = document.getElementById("myChart").getContext('2d');
+    var myChart = new Chart(ctx_one, {
         type: 'line',
         data: {
             labels: dates,
@@ -42,60 +122,6 @@ function main(){
 
 }
 
-function donut(){
-    var data = {
-          labels: [
-            "Red",
-            "Blue",
-            "Yellow"
-          ],
-          datasets: [
-            {
-              data: [300, 50, 100],
-              backgroundColor: [
-                "#FF6384",
-                "#36A2EB",
-                "#FFCE56"
-              ],
-              hoverBackgroundColor: [
-                "#FF6384",
-                "#36A2EB",
-                "#FFCE56"
-              ]
-            }]
-        };
-
-        var promisedDeliveryChart = new Chart(document.getElementById('Donut'), {
-          type: 'doughnut',
-          data: data,
-          options: {
-            responsive: true,
-            legend: {
-              display: false
-            }
-          }
-        });
-
-        Chart.pluginService.register({
-          beforeDraw: function(chart) {
-            var width = chart.chart.width,
-                height = chart.chart.height,
-                ctx = chart.chart.ctx;
-
-            ctx.restore();
-            var fontSize = (height / 114).toFixed(2);
-            ctx.font = fontSize + "em sans-serif";
-            ctx.textBaseline = "middle";
-
-            var text = "75%",
-                textX = Math.round((width - ctx.measureText(text).width) / 2),
-                textY = height / 2;
-
-            ctx.fillText(text, textX, textY);
-            ctx.save();
-          }
-        });
-}
 
 function updateData(){
 	$.getJSON(link+'/api/data', function(data) {
@@ -107,7 +133,7 @@ function updateData(){
         $("#data").text(data.length);
 
         // Donut chart
-        donut();
+
 
         // Historical Data Chart
         depths = [];
