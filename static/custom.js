e53d796b4c012657923ee75f44301010b7b8989c
Giorgi <giorgi.kharshila@gmail.com>
2018-04-09 22:16:24 -0400

    Modified chart

--- static/custom.js
+++ static/custom.js
@@ -42,17 +42,76 @@ function main(){
 
 }
 
+function donut(){
+    var data = {
+          labels: [
+            "Red",
+            "Blue",
+            "Yellow"
+          ],
+          datasets: [
+            {
+              data: [300, 50, 100],
+              backgroundColor: [
+                "#FF6384",
+                "#36A2EB",
+                "#FFCE56"
+              ],
+              hoverBackgroundColor: [
+                "#FF6384",
+                "#36A2EB",
+                "#FFCE56"
+              ]
+            }]
+        };
+
+        var promisedDeliveryChart = new Chart(document.getElementById('Donut'), {
+          type: 'doughnut',
+          data: data,
+          options: {
+            responsive: true,
+            legend: {
+              display: false
+            }
+          }
+        });
+
+        Chart.pluginService.register({
+          beforeDraw: function(chart) {
+            var width = chart.chart.width,
+                height = chart.chart.height,
+                ctx = chart.chart.ctx;
+
+            ctx.restore();
+            var fontSize = (height / 114).toFixed(2);
+            ctx.font = fontSize + "em sans-serif";
+            ctx.textBaseline = "middle";
+
+            var text = "75%",
+                textX = Math.round((width - ctx.measureText(text).width) / 2),
+                textY = height / 2;
+
+            ctx.fillText(text, textX, textY);
+            ctx.save();
+          }
+        });
+}
+
 function updateData(){
 	$.getJSON(link+'/api/data', function(data) {
     //data is the JSON string
     
     if (data.length != parseInt($("#data").text())) {
-    	$("#data").text(data.length);
+    	
+        // Number of items
+        $("#data").text(data.length);
 
+        // Donut chart
+        donut();
+
+        // Historical Data Chart
         depths = [];
         dates = [];
-
-        // console.log(data);
         var counter = 0;
     	for (var i in data){
     		// Depth in CM
