9aaaefff3b0828d9a2786345bba8fcda0dab9a74
Giorgi <giorgi.kharshila@gmail.com>
2018-04-14 15:06:47 -0400

    merge conflict should be solved

---
+++ app.py
@@ -0,0 +1,55 @@
+import bottle
+from bottle import response, static_file
+from database import *
+from datetime import datetime, timezone
+import os
+import json
+
+from sys import argv
+
+HOST = "127.0.0.1"
+PORT = argv[1]
+
+def authenticate(username, password):
+  if username == "foo" and password == "abcd.123":
+    return True
+  else:
+    return False
+
+@bottle.route('/static/<filepath:path>')
+def server_static(filepath):
+    return static_file(filepath, root='static/')
+
+@bottle.route("/", method="GET")
+def index():
+  return bottle.template("index.html")
+
+@bottle.route("/api/data", method="GET")
+def api_data():
+  response.headers['Content-Type'] = 'application/json'
+  response.headers['Cache-Control'] = 'no-cache'
+  return json.dumps(dataset())
+
+@bottle.route("/api/current", method="GET")
+def api_current():
+  response.headers['Content-Type'] = 'application/json'
+  response.headers['Cache-Control'] = 'no-cache'
+  return json.dumps(current())
+
+@bottle.route("/data", method="POST")
+@bottle.auth_basic(authenticate)
+def data():
+  username, password = bottle.request.auth
+  data_value = bottle.request.json
+
+  conn, cursor = connect_to_db()
+  cursor.execute("Insert into readings values(%d, '%s')" % (data_value["Data"], format_date(utc_to_local(datetime.now()))))
+  conn.commit()
+  disconnect(conn,cursor)
+  print("ADDED", data_value["Data"])
+  bottle.response.status = 200
+  return {"ADDED": data_value["Data"]}
+
+# Run the WebApp
+bottle.run(host=HOST, port=PORT)
+
---
+++ database.py
@@ -0,0 +1,50 @@
+import psycopg2
+from os import environ 
+
+def get_my_key(filename):
+    with open(filename) as f:
+        return f.read()
+
+def get_db_param():
+    url = get_my_key("key.txt").split('//')[1]
+    auth, conn = url.split("@")
+    username, password = auth.split(":")
+    socket, database = conn.split("/")
+    host, port = socket.split(":")
+    return (database, username, password, host, port)
+
+def connect_to_db():
+    db_name, db_username, db_passw, db_host, db_port = get_db_param()
+    try:
+        conn = psycopg2.connect(dbname=db_name, user=db_username, password=db_passw, host=db_host, port=db_port)
+        cursor = conn.cursor()
+        return (conn, cursor)
+    except:
+        print("Failed to connect to database")
+        exit()
+
+def disconnect(conn, cursor):
+    cursor.close()
+    conn.close()
+
+def dataset():
+    conn, curs = connect_to_db()
+    curs.execute("Select * from readings order by id desc;")
+    rows = curs.fetchall()
+    disconnect(conn,curs)
+    return rows
+
+def current():
+    return dataset()[0]
+
+def custom_query():
+    conn, curs = connect_to_db()
+    curs.execute("Select count(data) from readings;")
+    rows = curs.fetchall()
+    disconnect(conn,curs)
+    return rows
+def format_date(date):
+    return (str(date.year) + "/" + str(date.month).zfill(2) + "/" + str(date.day).zfill(2) + " " + str(date.hour).zfill(2) + ":" + str(date.minute).zfill(2) + ":" + str(date.second).zfill(2))
+
+def utc_to_local(utc_dt):
+    return utc_dt.replace(tzinfo=timezone.utc).astimezone(tz=None)
